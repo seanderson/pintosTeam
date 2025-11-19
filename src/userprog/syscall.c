c03aa375a376bc6d9c21834228c00e97911e7122
Ham3ed <ha1765@bard.edu>
2025-11-18 23:22:24 -0500

    Update syscall.c
    completed verigy_user() mapping part and included "userprog/pagedir.h" in header -(HA)

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,42 +6,44 @@
 #include "threads/vaddr.h"
 //+SEA
 #include "devices/shutdown.h"
+#include "userprog/pagedir.h"
 
-static void syscall_handler (struct intr_frame *);
-//void exit (int status);
-void s_exit ();
-void s_halt ();
+static void syscall_handler(struct intr_frame *);
+// void exit (int status);
+void s_exit();
+void s_halt();
 void s_write(void *sp);
-static bool verify_user (const uint8_t *uaddr);
+static bool verify_user(const uint8_t *uaddr);
 
-void
-syscall_init (void) 
+void syscall_init(void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
-  //intr_register_int (SYS_EXIT, 3, INTR_ON, exit, "exit");
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
+  // intr_register_int (SYS_EXIT, 3, INTR_ON, exit, "exit");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler(struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
+  printf("system call!\n");
   // Extract system call number from stack.
-  if (!verify_user(f->esp)) {
+  if (!verify_user(f->esp))
+  {
     printf("ERROR.  Bad user address.\n");
     thread_exit();
   }
-  int call_num = (int) *(int *)(f->esp);
-  printf("Call num %d\n",call_num);
-  switch (call_num) {
-    case SYS_HALT:
+  int call_num = (int)*(int *)(f->esp);
+  printf("Call num %d\n", call_num);
+  switch (call_num)
+  {
+  case SYS_HALT:
     printf("Sys halt\n");
     s_halt();
     break;
   case SYS_EXIT:
-    printf ("Sys exit detected.\n");
-    printf("Status: %d\n", (int) *(int *)(f->esp + 4));
-    f->eax = (int) *(int *)(f->esp + 4); // set return value
-    s_exit ();
+    printf("Sys exit detected.\n");
+    printf("Status: %d\n", (int)*(int *)(f->esp + 4));
+    f->eax = (int)*(int *)(f->esp + 4); // set return value
+    s_exit();
     break;
   case SYS_EXEC:
     break;
@@ -70,25 +72,28 @@ syscall_handler (struct intr_frame *f UNUSED)
   default:
     printf("Sys call is unknown!!\n");
   }
-  thread_exit ();
+  thread_exit();
 }
 
-void s_exit () {
+void s_exit()
+{
   printf("got to sysexit\n");
   thread_exit();
 }
 
-void s_halt () {
+void s_halt()
+{
   printf("got to syshalt\n");
   shutdown_power_off();
 }
 
-void s_write(void *sp) {
+void s_write(void *sp)
+{
   char *buf = (char *)(sp + 12);
   int n = (int)(int *)(sp + 8);
-  printf("num %d\n",n);
+  printf("num %d\n", n);
 
-  //putbuf (buf,n);
+  // putbuf (buf,n);
 }
 
 /* Validate data user virtual address uaddr.
@@ -98,11 +103,19 @@ void s_write(void *sp) {
    Returns true if successful, false if not.
    +SEA
 */
-static bool 
-verify_user (const uint8_t *uaddr)
+static bool
+verify_user(const uint8_t *uaddr)
 {
   int result;
-  if (uaddr == NULL || uaddr >= PHYS_BASE) return false;
+  if (uaddr == NULL || uaddr >= PHYS_BASE)
+    return false;
   // is it mapped?  How do I check this?
+  // to check if the address is mapped if not return false
+  void *page = pagedir_get_page(thread_current()->pagedir, uaddr);
+  if (page == NULL)
+  {
+    return false;
+  }
+
   return true;
 }
