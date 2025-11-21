dad97d1cb66cf47a09071a06f0d946485a0b2edc
Ham3ed <ha1765@bard.edu>
2025-11-20 22:10:10 -0500

    HA_ Update2 of syscall.c
    HA - Explained memory layout and need for checking, also updated the verify_user() method

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -96,6 +96,20 @@ void s_write(void *sp)
   // putbuf (buf,n);
 }
 
+/*
+Virtual Memory Layout:
+
+virtual memory in pintos devided in two regions: user virtual memory (0 up to PHYS_BASE (3GB)) and kernel virtual memory(PHYS_BASE up to 4GB). User virtual memory is per-process. when the kernel switches from one process to another, it also switches user virtual address spaces by changing processor's page directory base register. Kernel virtual memory is global. It is always mapped the same way, regardless of what user process or kernel thread is running. A user program can only access its own virtual memory, any attempt to access kernel virtual memory causes a page fault. Kernel can access both its own virtual memory and virtual memory of a running user process. However even in kernel an attempt to access memory of an unmapped user virtual address will cause page fault.
+
+
+
+Need for checking:
+As part a system call, the kernel must often access memory through pointers provided by the user program. The user could pass a null pointer, a pointer to unmapped virtual memory or a pointer to kernel virtual memory space. All mentioned pointers are harmful to kernel an could cause crush and pagefault, basically by checking we make sure that the pointers are valid and will not harm the kernel.
+
+Pintos: chap3: 3.1.4 - 3.1.5
+
+*/
+
 /* Validate data user virtual address uaddr.
    UADDR must be below PHYS_BASE.
    It must not be null.
@@ -107,15 +121,14 @@ static bool
 verify_user(const uint8_t *uaddr)
 {
   int result;
-  if (uaddr == NULL || uaddr >= PHYS_BASE)
-    return false;
+  // if (uaddr == NULL || uaddr >= PHYS_BASE)
+  //   return false;
   // is it mapped?  How do I check this?
   // to check if the address is mapped if not return false
-  void *page = pagedir_get_page(thread_current()->pagedir, uaddr);
-  if (page == NULL)
-  {
-    return false;
-  }
 
-  return true;
+  // to check the VADDR is user virtual address
+  if (!is_user_vaddr(uaddr))
+    return false;
+  // retuns false if the address in not mapped
+  return pagedir_get_page(thread_current()->pagedir, uaddr) != NULL;
 }
